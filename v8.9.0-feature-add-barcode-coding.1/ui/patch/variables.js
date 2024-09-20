4483774d7f1ac4fddbb77e1c306df18dfc063f7f
s-continuum, IT265 <noreply-jenkins@post.ch>
2024-09-20 09:48:12 +0200

    v8.9.0-feature-add-barcode-coding.1

---
+++ v8.9.0-feature-add-barcode-coding.1/ui/patch/variables.js
@@ -0,0 +1,55 @@
+var variables={
+  "version": "8.9.0-feature-add-barcode-coding.1",
+  "name": "@ch-post-app-cs/coding-sorting-stream-api",
+  "description": "Coding Sorting Stream API",
+  "scripts": {
+    "start": "apikana start src dist",
+    "stop": "apikana stop",
+    "prepublishOnly": "npm test",
+    "create-sample": "apikana create-sample",
+    "validate-samples": "apikana validate-samples",
+    "test": "eslint src --ext .ts && apikana validate-samples",
+    "lint": "eslint src --ext .ts",
+    "lint-fix": "eslint src --ext .ts --fix"
+  },
+  "author": "gfellerle",
+  "license": "Apache-2.0",
+  "devDependencies": {
+    "@typescript-eslint/eslint-plugin": "^4.13.0",
+    "@typescript-eslint/parser": "^4.13.0",
+    "apikana": "0.9.34",
+    "apikana-defaults": "0.0.96",
+    "eslint": "^7.17.0",
+    "eslint-config-prettier": "^7.1.0",
+    "eslint-plugin-prettier": "^3.3.1",
+    "prettier": "^2.8.8",
+    "typescript": "^4.1.3"
+  },
+  "customConfig": {
+    "type": "stream-api",
+    "domain": "post.ch",
+    "author": "Team Erkennen Codieren Pakete",
+    "namespace": "app.cs.coding.sorting",
+    "shortName": "coding-sorting",
+    "projectName": "coding-sorting-stream-api",
+    "title": "Coding Sorting Stream API",
+    "plugins": [
+      "maven",
+      "dotnet",
+      "readme"
+    ],
+    "javaPackage": "ch.post.app.cs.coding.sorting.v1",
+    "mavenGroupId": "ch.post.app.cs.coding",
+    "dotnetNamespace": "Ch.Post.App.Cs.Coding.Sorting.V1",
+    "dotnetPackageId": "Ch.Post.App.Cs.Coding.Sorting.StreamApi",
+    "mqs": "MQTT",
+    "avro": {
+      "enumAsString": true
+    }
+  },
+  "_": [
+    "start",
+    "src",
+    "dist"
+  ]
+}
